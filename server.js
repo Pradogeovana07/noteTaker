7626dd6ad7f4e91fb749407feadbc2c166ba4e07
Geo_K <113002223+Pradogeovana07@users.noreply.github.com>
2023-02-21 00:42:58 -0700

    is now listening in PORT3001

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json')
+const fs = require('fs')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+app.get('/api/notes', (req, res) =>
+  res.json(notes)
+);
+app.post('/api/notes', (req, res) =>{
+notes.push(req.body)
+fs.writeFileSync('/db/db.json', JSON.stringify(notes))
+  res.json(notes)
+});
+app.listen(PORT,()=> console.log(`listeningOnPort: ${PORT}`))
